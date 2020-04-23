31f65ec83ae53a8d8d114d336940c45eed97fe8d
Awsafur Rahman <36858976+awsaf49@users.noreply.github.com>
2020-04-23 20:28:21 +0600

    Update setup.py

--- aws/setup.py
+++ aws/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
-    name='Measurements',
+    name='aws',
     url='https://github.com/awsaf49/my-package',
     author='awsaf rahman',
     author_email='awsaf49@gmail.com',
