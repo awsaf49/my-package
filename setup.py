48e553fe2e26f44d5af5dcf4644617993518d840
Awsafur Rahman <36858976+awsaf49@users.noreply.github.com>
2020-04-23 20:57:31 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,12 @@
 from setuptools import setup
 
 setup(
-    # Needed to silence warnings (and to be a worthwhile package)
     name='aws',
-    url='https://github.com/awsaf49/my-package',
-    author='awsaf rahman',
-    author_email='awsaf49@gmail.com',
-    # Needed to actually package something
+    version='0.1',
     packages=['aws'],
-    # Needed for dependencies
-    install_requires=['numpy','panda'],
-    # *strongly* suggested for sharing
-    version='1.0',
-    # The license can be anything you like
+    url='https://github.com/awsaf49/my-package',
     license='MIT',
-    description='An example of a python package from pre-existing code',
-    # We will also need a readme eventually (there will be a warning)
-    long_description=open('README.txt').read(),
+    author='awsaf',
+    author_email='awsaf49@gmail.com',
+    description='My packages'
 )
