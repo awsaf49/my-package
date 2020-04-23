e93840a1f5e5413879fab1397f35b00df6a76e62
Awsafur Rahman <36858976+awsaf49@users.noreply.github.com>
2020-04-23 20:41:07 +0600

    Update and rename aws/setup.py to setup.py

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ setup(
     # Needed to actually package something
     packages=['aws'],
     # Needed for dependencies
-    install_requires=['numpy'],
+    install_requires=['numpy','panda'],
     # *strongly* suggested for sharing
-    version='0.1',
+    version='1.0',
     # The license can be anything you like
     license='MIT',
     description='An example of a python package from pre-existing code',
     # We will also need a readme eventually (there will be a warning)
-    # long_description=open('README.txt').read(),
+    long_description=open('README.txt').read(),
 )
