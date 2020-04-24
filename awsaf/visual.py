06f5d8410601308d93214a30da26a8a48cfe02a3
Awsafur Rahman <36858976+awsaf49@users.noreply.github.com>
2020-04-24 06:41:06 +0600

    Update visual.py

--- awsaf/visual.py
+++ awsaf/visual.py
@@ -1,45 +1,45 @@
-# all necessary packages
-import keras
-import tensorflow as tf
-from keras.layers import *
-from keras.models import *
-from keras.optimizers import *
-from keras.callbacks import *
-from keras.utils import *
-from keras.applications.resnet50 import ResNet50
-from keras.applications.vgg16 import VGG16
-from keras.applications.vgg19 import VGG19
-from keras.applications.inception_v3 import InceptionV3
-from keras.applications.xception import Xception
-from keras.applications.densenet import DenseNet121
-from keras import backend as K
+# # all necessary packages
+# import keras
+# import tensorflow as tf
+# from keras.layers import *
+# from keras.models import *
+# from keras.optimizers import *
+# from keras.callbacks import *
+# from keras.utils import *
+# from keras.applications.resnet50 import ResNet50
+# from keras.applications.vgg16 import VGG16
+# from keras.applications.vgg19 import VGG19
+# from keras.applications.inception_v3 import InceptionV3
+# from keras.applications.xception import Xception
+# from keras.applications.densenet import DenseNet121
+# from keras import backend as K
 
-import cv2
-from PIL import Image
-from matplotlib import pyplot as plt
-from keras.preprocessing.image import load_img, img_to_array
-from keras.preprocessing.image import ImageDataGenerator
+# import cv2
+# from PIL import Image
+# from matplotlib import pyplot as plt
+# from keras.preprocessing.image import load_img, img_to_array
+# from keras.preprocessing.image import ImageDataGenerator
 
-import numpy as np
-import pandas as pd
-import os
-from glob import glob
-from tqdm import tqdm
-from sklearn.model_selection   import train_test_split
-import itertools
-from sklearn.metrics import *
-
-
+# import numpy as np
+# import pandas as pd
+# import os
+# from glob import glob
+# from tqdm import tqdm
+# from sklearn.model_selection   import train_test_split
+# import itertools
+# from sklearn.metrics import *
 
 
-# plot confusion matrix
 
-import itertools
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix
-import pandas.util.testing as tm
-from sklearn import metrics
+
+# # plot confusion matrix
+
+# import itertools
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from sklearn.metrics import confusion_matrix
+# import pandas.util.testing as tm
+# from sklearn import metrics
 import seaborn as sns
 sns.set()
 
@@ -502,7 +502,7 @@ def grad_cam(img_path , model , last_conv_layer_name,  image_size = (256, 256), 
   cv2.imwrite('grad_cam_result.png', superimposed_img) # otherwise it'll show error because of float
   grad_img = cv2.imread('grad_cam_result.png')
   grad_img = cv2.cvtColor(grad_img, cv2.COLOR_BGR2RGB)
-#     heatmap = cv2.cvtColor(heatmap, cv2.COLOR_BGR2RGB)
+
 
 
   return ori_img, heatmap_img, grad_img
