3d83709f5d14ece562ac008779665babf15e0501
Awsafur Rahman <36858976+awsaf49@users.noreply.github.com>
2020-04-25 16:58:33 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='awsaf',
-    version='1.0',
+    version='1.2',
     packages=['awsaf'],
     url='https://github.com/awsaf49/my-package',
     license='MIT',
