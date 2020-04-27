3aa98f46cee1db668249e5db821f3a7d0e44b31f
Awsafur Rahman <36858976+awsaf49@users.noreply.github.com>
2020-04-28 05:34:14 +0600

    Update segmentation.py

--- awsaf/segmentation.py
+++ awsaf/segmentation.py
@@ -12,7 +12,7 @@ def dice_coef(y_true, y_pred, smooth=1e-5):
     return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
 
 def dice_loss(y_true, y_pred):
-    smooth = 1e-5.
+    smooth = 1e-5
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
     intersection = y_true_f * y_pred_f
