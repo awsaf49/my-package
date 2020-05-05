b63a9b003d905b7e0064be8276f9abd14f61d239
Awsafur Rahman <36858976+awsaf49@users.noreply.github.com>
2020-05-05 20:59:26 +0600

    Update models.py

--- awsaf/models.py
+++ awsaf/models.py
@@ -551,3 +551,60 @@ def CovXNet32(input_shape, nb_class, depth):
   model.compile(loss='categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
 
   return model
+
+
+def Unet(input_shape=(256, 256, 3)):
+  
+  from .segmentation import bce_dice_loss, dice_coef, jaccard
+  from keras.layers import Dense, Dropout, Conv2D, BatchNormalization, SeparableConv2D, Concatenate, Conv2DTranspose
+  from keras.layers import MaxPooling2D, Input, Add, DepthwiseConv2D, GlobalAveragePooling2D, Flatten, concatenate
+  from keras.models import Model, load_model
+  from keras.utils import plot_model
+  from keras.optimizers import Adam, SGD, RMSprop  
+  
+  inputs = Input(shape = input_shape)
+
+  conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
+  conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
+  pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+  conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
+  conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
+  pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+  conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
+  conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
+  pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+  conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool3)
+  conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4)
+  pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+
+  conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(pool4)
+  conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv5)
+
+  up6 = concatenate([Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(conv5), conv4], axis=3)
+  conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(up6)
+  conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv6)
+
+  up7 = concatenate([Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(conv6), conv3], axis=3)
+  conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(up7)
+  conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv7)
+
+  up8 = concatenate([Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(conv7), conv2], axis=3)
+  conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(up8)
+  conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv8)
+
+  up9 = concatenate([Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(conv8), conv1], axis=3)
+  conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(up9)
+  conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv9)
+
+  conv10 = Conv2D(1, (1, 1), activation='sigmoid')(conv9)
+
+  model = Model(inputs=[inputs], outputs=[conv10])
+  
+  model.compile(loss = bce_dice_loss, metrics = [dice_coef, jaccard], optimizer = Adam(lr = 1e-4))
+  
+  return model
+  
+ 
