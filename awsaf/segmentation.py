21782adaa721a64da713c4842254b4a862792682
Awsafur Rahman <36858976+awsaf49@users.noreply.github.com>
2020-05-06 07:09:45 +0600

    color map of visualize has been updated

--- awsaf/segmentation.py
+++ awsaf/segmentation.py
@@ -325,7 +325,7 @@ def create_epoch_plot_df(df):
     
     
 
-def visualize(model, test_generator, n_outputs = 5, thr = 0.5):
+def visualize(model, test_generator, n_outputs = 5, thr = 0.5, mask_cmap = 'autumn', pred_cmap = 'winter'):
     
     import matplotlib.pyplot as plt
     import seaborn as sns
@@ -351,13 +351,13 @@ def visualize(model, test_generator, n_outputs = 5, thr = 0.5):
         ax[idx][1].imshow(test_image[idx,:,:,0], cmap = 'bone')
         mask = test_mask[idx,:,:]
         mask = np.ma.masked_where(mask==0, mask)
-        ax[idx][1].imshow(mask, cmap = 'autumn', alpha = 0.7)
+        ax[idx][1].imshow(mask, cmap = mask_cmap, alpha = 0.7)
         ax[idx][1].set_title('Segmentaton:')
 
         ax[idx][2].imshow(test_image[idx,:,:,0], cmap = 'bone')
         p_mask = pred_mask[idx,:,:]
         p_mask = np.ma.masked_where(p_mask==0, p_mask)
-        ax[idx][2].imshow(p_mask, cmap = 'winter', alpha = 0.8)
+        ax[idx][2].imshow(p_mask, cmap = pred_cmap, alpha = 0.8)
         ax[idx][2].set_title('Prediction:')
     
 
